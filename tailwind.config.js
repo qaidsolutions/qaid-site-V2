2fb980b5a9a05c21e1840198964b733aeefee38b
qaidsolutions <qaid.solutions@gmail.com>
2025-11-06 21:14:29 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        qaidBlue: "#64B5F6",
+        qaidTeal: "#007B7F",
+        qaidDark: "#004F54"
+      },
+      fontFamily: {
+        mont: ["Montserrat", "sans-serif"],
+        open: ["Open Sans", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+}
